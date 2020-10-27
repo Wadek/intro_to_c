c0d7225c18f5a80116d35269d92f7ad3dfcbb5f9
wadek221 <wkariniemi@gmail.com>
2020-10-27 17:17:02 +0200

    using scanf

---
+++ circle_area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+	int rad;
+	printf("please ente a raduis: ");
+	scanf("%i", &rad);
+
+	printf("The given radius is: %i\n", rad);
+	return 0;
+}
+
--- hello.c
+++ hello.c
@@ -1,6 +1,12 @@
 ﻿#include <stdio.h>
 
 int main() {
-	printf("hello world\n");
+  
+  int x;
+  x = 10;
+  
+  int y = x / 2;
+
+	printf("x is: %i\ny is: %i\n",x,y);
 	return 0;
 }
